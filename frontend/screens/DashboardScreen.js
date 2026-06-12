0ae47a0c3b321eebb209fe35eb0fb191fcb6b337
Sunhee Jung <sunhee8402@naver.com>
2026-06-12 12:18:15 +1000

    Update API endpoint for fetching projects

--- frontend/screens/DashboardScreen.js
+++ frontend/screens/DashboardScreen.js
@@ -20,7 +20,7 @@ const DashboardScreen = ({ navigation }) => {
 
   const fetchProjects = async () => {
     try {
-      const response = await fetch('http://localhost:5000/api/projects');
+      const response = await fetch('http://localhost:3001/api/projects');
       const data = await response.json();
       setProjects(data);
       setLoading(false);
